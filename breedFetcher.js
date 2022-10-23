89b45c9bd4384c07984a901b21afd8e0951d03ee
Hillary Forget <hillaryforget@outlook.com>
2022-10-23 13:37:25 -0400

    defined callback and call function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,39 +1,47 @@
+const request = require("request");
+const apiRequestURL = "https://api.thecatapi.com/v1/breeds/search?q=";
 
-
-const request = require('request');
-const apiRequestURL = 'https://api.thecatapi.com/v1/breeds/search?q=';
+const callback = (err, desc) => {
+  if (err) {
+    throw Error(err);
+  } else {
+    return console.log(desc);
+  }
+};
 
 //main request logic
 const fetchBreedDescription = (breedName, cb) => {
-  if (breedName !== null) {
-    return;
+  console.log('string');
+  if (!breedName) {
+    return console.log('Please provide breed name');
   }
 
   //make request
-  request((apiRequestURL + breedName), function(error, response, body) {
-    //console.log(typeof body);
+  request(apiRequestURL + breedName, function(error, response, body) {
+  
+    let desc = null;
+    let err = error;
+    if (error) {
+      cb(err, desc);
+    }
 
     //parse JSON string to object
     const data = JSON.parse(body);
-    //console.log(data);
-    //console.log(typeof data);
-    let err = error;
-    let desc = null;
-
     //check if it is empty
     if (data.length === 0) {
-      err = 'cat breed not found';
+      err = "cat breed not found";
       cb(err, desc);
       return;
     }
 
     //get the description
     desc = data[0].description;
-    //console.log(data[0].description);
 
     //call callback:
     cb(err, desc);
   });
 };
+
+fetchBreedDescription('Siberian', callback);
 
 module.exports = { fetchBreedDescription };
