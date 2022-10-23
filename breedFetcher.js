64ec92448945a83c95535e011d676ffedc5e0fa8
Hillary Forget <hillaryforget@outlook.com>
2022-10-23 08:23:35 -0400

    nothing is happening? added console.log

---
+++ breedFetcher.js
@@ -0,0 +1,52 @@
+
+
+const request = require('request');
+const apiRequestURL = 'https://api.thecatapi.com/v1/breeds/search?q=';
+
+const fetchBreed = (breedName, cb) => {
+  if (breedName !== null) {
+    return;
+  }
+
+  //make request
+  request((apiRequestURL + breedName), function(error, response, body) {
+    console.log(typeof body);
+
+    //parse JSON string to object
+    const data = JSON.parse(body);
+    console.log(data);
+    console.log(typeof data);
+    let err = error;
+    let desc = null;
+
+    //check if it is empty
+    if (data.length === 0) {
+      err = 'cat breed not found';
+      cb(err, desc);
+      return;
+    }
+
+    //get the description
+    desc = data[0].description;
+    console.log(data[0].description);
+
+    //call callback:
+    cb(err, desc);
+  });
+};
+
+module.exports = { fetchBreed };
+
+
+
+
+
+
+
+
+
+
+//const tags = JSON.parse(body);
+//console.log("Json parsed: ", tags, typeof tags);
+  
+
